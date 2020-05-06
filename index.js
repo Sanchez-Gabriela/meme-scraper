7b35a7a1e75db0aec7573e9defcc11a9b0f562d5
Gabriela Sanchez <gaby.peta@gmail.com>
2020-05-06 20:45:15 +0200

    ADD CHANGES

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const cheerio = require('cheerio');
+const got = require('got');
+
+const mainUrl = 'https://memegen.link/examples';
+
+got(mainUrl).then(response => {
+  const $ = cheerio.load(response.body);
+
+
+  $('img').each((i, img) => {
+    const src = img.attribs.src;
+    console.log('https://memegen.link' + src);
+  });
+}).catch(err => {
+  console.log(err);
+});
+
